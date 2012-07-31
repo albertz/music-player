860fd62d444123284ee3d965d8b26e33ae37ebd7
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-08-01 01:54:29 +0200

    more

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python
 
-queue = loadQueue()
-recentyplayedList = loadRecentlyplayedList()
-
-def play(song):
-	# via ffmpeg or so. load dynamically (ctypes)
-	pass
-
 def main():
 	while True:
 		play(head(queue))	
@@ -20,12 +13,37 @@ def tracker():
 	for ev in mainStateChanges():
 		track(ev)
 
-actions = (
-	"play",
-	"pause",
-	"next",
-	"forward-10s",
-)
+class Actions:
+	def play(self, song):
+		# via ffmpeg or so. load dynamically (ctypes)
+		pass
+		
+		
+	def pause(self): pass
+	def next(self): pass
+	def forward10s(self): pass
+
+actions = Actions()
+
+class initBy(property):
+	def __init__(self, initFunc):
+		property.__init__(self, fget = self.fget)
+		self.initFunc = initFunc
+	def fget(self, inst):
+		if hasattr(self, "value"): return self.value
+		self.value = self.initFunc()
+		return self.value
+	
+class State:
+	queue = initBy(loadQueue)
+	recentlyPlayedList = initBy(loadRecentlyplayedList)
+	
+	playState = oneOf(
+		"playing",
+		"paused"
+	)
+	
+state = State()
 
 if __name__ == '__main__':
 	startThread(tracker)
