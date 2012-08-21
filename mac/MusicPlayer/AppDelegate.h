6fd5a7760d5c8f4636ab9b21434732a2a42a3b67
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-08-21 14:46:26 +0200

    mac stuff

---
+++ mac/MusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 21.08.12.
+//  Copyright (c) 2012 Albert Zeyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ mac/MusicPlayer/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 21.08.12.
+//  Copyright (c) 2012 Albert Zeyer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
