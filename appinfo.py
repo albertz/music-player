c2dd8a15efe3238a6393dc27ebb0e79434867336
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-08-24 03:31:53 +0200

    appinfo

--- appinfo.py
+++ appinfo.py
@@ -4,7 +4,7 @@ progname = "MusicPlayer"
 appid = "com.albertzeyer." + progname
 version = "1.0"
 
-userdir = "~/." + progname
+userdir = "~/." + appid
 platform = None
 
 uname = os.uname()
@@ -12,14 +12,14 @@ if "iPhone" in uname[4] or "iPod" in uname[4]:
 	userdir = "~/Library/" + progname
 	platform = "iOS"	
 elif sys.platform == "darwin":
-	userdir = "~/Library/Application Support/" + progname
+	userdir = "~/Library/Application Support/" + appid
 	platform = "MacOSX"
 elif sys.platform == "win32":
 	from win32com.shell import shellcon, shell
 	userdir = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0) + "/" + progname
 	platform = "Win32"
 else:
-	print "warning: unknown platform", repr(sys.platform)
+	print "warning: unknown/untested platform", repr(sys.platform)
 
 userdir = os.path.expanduser(userdir)
 try: os.makedirs(userdir)
