a19613229a4314189e3f94a8dbf1aef80a48d9b6
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-08-24 03:09:53 +0200

    lastfm __init__

--- lastfm/__init__.py
+++ lastfm/__init__.py
@@ -1,3 +1,99 @@
-from __future__ import absolute_import
+import appinfo
+from dropbox import client, rest, session
 
-from . import client, rest, session
+# i don't really know how to make these secure...
+APP_KEY = 'xxx'
+APP_SECRET = 'xxx'
+ACCESS_TYPE = 'dropbox'
+
+
+class OAuthReturnHandler:
+	def __init__(oself):
+		oself.httpd_access_token_callback = None
+		
+		import BaseHTTPServer
+		class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+			def log_message(self, format, *args): pass
+			def do_GET(webself):
+				if webself.path.startswith("/get_access_token?"):
+					oself.httpd_access_token_callback = webself.path
+
+					webself.send_response(200)
+					webself.send_header("Content-type", "text/html")
+					webself.end_headers()
+					webself.wfile.write("""
+						<html><head><title>OAuth return</title></head>
+						<body onload="onLoad()">
+						<script type="text/javascript">
+						function onLoad() {
+							ww = window.open(window.location, "_self");
+							ww.close();
+						}
+						</script>
+						</body></html>""")
+				else:
+					webself.send_response(404)
+					webself.end_headers()
+
+		oself.handler = Handler		
+		def tryOrFail(fn):
+			try: fn(); return True
+			except: return False
+		# Try with some default ports first to avoid cluttering the users Google Authorized Access list.
+		tryOrFail(lambda: oself.startserver(port = 8123)) or \
+		tryOrFail(lambda: oself.startserver(port = 8321)) or \
+		oself.startserver(port = 0)
+
+		_,oself.port = oself.httpd.server_address
+		oself.oauth_callback_url = "http://localhost:%d/get_access_token" % oself.port
+
+	def startserver(self, port):
+		import BaseHTTPServer
+		self.httpd = BaseHTTPServer.HTTPServer(("", port), self.handler)
+
+	def wait_callback_response(self):
+		while self.httpd_access_token_callback == None:
+			self.httpd.handle_request()
+		return self.httpd_access_token_callback
+
+class StoredSession(session.DropboxSession):
+	"""a wrapper around DropboxSession that stores a token to a file on disk"""
+	TOKEN_FILE = appinfo.userdir + "/dropbox_session_token_store.txt"
+	
+	def load_creds(self):
+		try:
+			stored_creds = open(self.TOKEN_FILE).read()
+			self.set_token(*stored_creds.split('|'))
+			print "* loaded access token"
+		except IOError:
+			pass # don't worry if it's not there
+	
+	def write_creds(self, token):
+		f = open(self.TOKEN_FILE, 'w')
+		f.write("|".join([token.key, token.secret]))
+		f.close()
+
+	def delete_creds(self):
+		os.unlink(self.TOKEN_FILE)
+
+	def link(self):
+		request_token = self.obtain_request_token()
+		oauthreturnhandler = OAuthReturnHandler()
+
+		url = self.build_authorize_url(
+			request_token, oauth_callback = oauthreturnhandler.oauth_callback_url)
+		
+		print "* open oauth login page"
+		import webbrowser; webbrowser.open(url)
+		
+		print "* waiting for redirect callback ...",
+		httpd_access_token_callback = oauthreturnhandler.wait_callback_response()
+		print "done"
+	
+		self.obtain_access_token(request_token)
+		self.write_creds(self.token)
+	
+	def unlink(self):
+		self.delete_creds()
+		session.DropboxSession.unlink(self)
+
