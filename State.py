7b707464f33d8ed1ebd80e667efd4342af4effc3
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-08-24 07:48:13 +0200

    use RandomFileQueue. first very simple but useable music player!

--- State.py
+++ State.py
@@ -5,23 +5,17 @@ player = None
 
 def loadQueue(state):
 	print "load queue"
-
-	# dummy example from test_ffmpeg
+	
 	def songs():
-		files = [
-			"/Users/az/Music/Classic/Glenn Gould Plays Bach/Two- & Three-Part Inventions - Gould/19 Bach - Invention 13 in a (BWV 784).mp3",
-			"/Users/az/Music/Rock/Tool/Lateralus/09 Lateralus.flac",
-			"/Users/az/Music/Cults - Cults 7/Cults - Cults 7- - 03 The Curse.flac",
-			"/Users/az/Music/Special/zorba/(01) - Theme From Zorba The Greek.ogg",
-			"/Users/az/Music/Classic/Glenn Gould Plays Bach/French Suites, BWV812-7 - Gould/Bach, French Suite 5 in G, BWV816 - 5 Bourree.mp3",
-			"/Users/az/Music/Electronic/Von Paul Kalkbrenner - Aaron.mp3",
-			"/Users/az/Music/Electronic/One Day_Reckoning Song (Wankelmut Remix) - Asaf Avidan & the Mojos.mp3",
-		]
-		i = 0
+		import os
+		from RandomFileQueue import RandomFileQueue
+		fileQueue = RandomFileQueue(
+			rootdir = os.path.expanduser("~/Music"),
+			fileexts = ["mp3", "ogg", "flac"])
 		while True:
-			yield Song(files[i], state._main["player"])
-			i += 1
-			if i >= len(files): i = 0
+			f = fileQueue.getNextFile()
+			yield Song(f, state._main["player"])
+			
 	return songs()
 	
 def loadRecentlyplayedList(state):
