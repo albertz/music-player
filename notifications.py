e71e65d30de5246aa74b99a60586b14f14b4a57f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-09-02 07:47:08 +0200

    notifications module

---
+++ notifications.py
@@ -0,0 +1,17 @@
+def notificationsMain():
+	from State import state
+	from player import PlayerEventCallbacks
+	import AppKit
+
+	pool = AppKit.NSAutoreleasePool.alloc().init()
+
+	notifCenter = AppKit.NSUserNotificationCenter.defaultUserNotificationCenter()
+	for ev,args,kwargs in state.updates.read():
+		if ev is PlayerEventCallbacks.onSongChange:
+			notif = AppKit.NSUserNotification.alloc().init()
+			notif.setTitle_("MusicPlayer")
+			notif.setInformativeText_(kwargs["newSong"].userString)
+			notifCenter.deliverNotification_(notif)
+			#print "notif:", notif
+
+	del pool
