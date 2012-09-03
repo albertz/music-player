4d5b2c0d051d3022ba08127c56fc9d6187e4c576
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-09-03 03:29:24 +0200

    mac main.m

--- mac/MusicPlayer/main.m
+++ mac/MusicPlayer/main.m
@@ -7,8 +7,40 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <Python/Python.h>
+
+static void addPyPath() {
+	NSString* pathStr =
+	[[NSString alloc]
+	 initWithFormat:@"%s:%s:%@/Python",
+	 Py_GetPath(),
+	 "/System/Library/Frameworks/Python.framework/Versions/Current/Extras/lib/python/PyObjC",
+	 [[NSBundle mainBundle] resourcePath]];
+	PySys_SetPath((char*)[pathStr UTF8String]);
+	[pathStr release];
+}
 
 int main(int argc, char *argv[])
 {
-	return NSApplicationMain(argc, (const char **)argv);
+	//return NSApplicationMain(argc, (const char **)argv);
+
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	NSString* mainPyFilename = [[[NSBundle mainBundle] resourcePath] stringByAppendingString:@"/Python/main.py"];
+	NSLog(@"main: %@", mainPyFilename);
+	Py_SetProgramName((char*)[mainPyFilename UTF8String]);
+	
+	Py_Initialize();
+	addPyPath();
+	
+	// maybe PySys_SetArgvEx ?
+	
+	PyRun_SimpleString("print 'hello there'");
+		
+	FILE* fp = fopen((char*)[mainPyFilename UTF8String], "r");
+	assert(fp);
+	PyRun_SimpleFile(fp, "main.py");
+	
+	[pool release];
+	return 0;
 }
