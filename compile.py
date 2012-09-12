79de416bdb682a40c4aff10e89f469fbb52c4438
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-09-12 16:15:40 +0200

    compile.py: import chromaprint

--- compile.py
+++ compile.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python
 
 import os, sys
+from glob import glob
+os.chdir(os.path.dirname(__file__))
 
 def sysExec(cmd):
     print " ".join(cmd)
     r = os.system(" ".join(cmd))
     if r != 0: sys.exit(r)
-    
-sysExec(["gcc", "-c", "ffmpeg.c", "-I", "/System/Library/Frameworks/Python.framework/Headers/", "-g"])
-sysExec(["libtool", "-dynamic", "-o", "ffmpeg.so", "ffmpeg.o",
-    "-framework", "Python",
-    "-lavformat", "-lavutil", "-lavcodec", "-lswresample", "-lportaudio", "-lc"])
+
+sysExec(["mkdir","-p","build"])
+os.chdir("build")
+
+sysExec(["gcc", "-c"] +
+	["../ffmpeg.c"] +
+	glob("../chromaprint/*.cpp") +
+	[
+	"-DHAVE_CONFIG_H",
+	"-I", "/System/Library/Frameworks/Python.framework/Headers/",
+	"-I", "../chromaprint",
+	"-g",
+	])
+
+sysExec(["libtool", "-dynamic", "-o", "../ffmpeg.so"] +
+	glob("*.o") +
+	[
+	"-framework", "Python",
+	"-lavformat", "-lavutil", "-lavcodec", "-lswresample", "-lportaudio",
+	"-lc", "-lstdc++",
+	])
