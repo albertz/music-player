4cd484fc1335da5201c1893b9cb0e80a6085985b
Bryan Joseph <bryanjos@gmail.com>
2012-09-18 20:03:26 -0500

    Added in another DB implementation using sqlite

--- SongStore.py
+++ SongStore.py
@@ -1,4 +1,5 @@
 from Song import Song
+import sqlite3
 
 # http://code.google.com/p/leveldb/
 # http://code.google.com/p/py-leveldb/
@@ -12,10 +13,107 @@ def dbRepr(o): return utils.betterRepr(o)
 
 def dbUnRepr(s): return eval(s)
 
+class SqliteDB:
+    def __init__(self, tableName):
+        self.tableName = tableName
+        self.dbPath = appinfo.userdir + "/db.sqlite"
+
+        conn = sqlite3.connect(self.dbPath)
+        c = conn.cursor()
+
+        sql = 'create table if not exists ' + self.tableName + ' (key Text primary key, value Text)'
+        c.execute(sql)
+
+        conn.commit()
+        c.close()
+
+    def __getitem__(self, item):
+        conn = sqlite3.connect(self.dbPath)
+        conn.text_factory = str
+        c = conn.cursor()
+
+        c.execute('select value from ' + self.tableName + ' where key = ?', (dbRepr(item),))
+
+        result = c.fetchall()
+
+        if len(result) > 0:
+            c.close()
+            return dbUnRepr(result[0][0])
+
+        c.close()
+        return None
+
+
+    def __setitem__(self, key, value):
+        conn = sqlite3.connect(self.dbPath)
+        conn.text_factory = str
+        c = conn.cursor()
+
+        repKey = dbRepr(key)
+        repVal = dbRepr(value)
+
+        c.execute('select key, value from ' + self.tableName + ' where key = ?', (repKey, ))
+
+        result = c.fetchall()
+
+        if len(result) > 0:
+            c.execute('update ' + self.tableName + ' set value = ? where key = ?',(repVal, repKey,))
+        else:
+            c.execute('insert into ' + self.tableName + ' values(?,?)',(repKey, repVal,))
+
+        conn.commit()
+        c.close()
+
+    def __delitem__(self, key):
+        conn = sqlite3.connect(self.dbPath)
+        conn.text_factory = str
+        c = conn.cursor()
+
+        c.execute('delete from ' + self.tableName + ' where key = ?',(dbRepr(key),))
+
+        conn.commit()
+        c.close()
+
+    def setDefault(self, key, value):
+        if self[key] is not None:
+            return self[key]
+        else:
+            self[key] = value
+            return self[key]
+
+    #If not value set at key, it creates a set. Will append value to set
+    def setAppend(self, key, valueToAppend):
+        value = self.setDefault(key, set())
+        value.add(valueToAppend)
+        self[key] = value
+        return value
+
+    #if key not set does nothing. If it is set then remove the value from the key. If key is empty, removes key
+    def setRemove(self, key, valueToRemove):
+        if self[key] is not None:
+            value = self[key]
+            value.remove(valueToRemove)
+            if len(value) == 0:
+                del self[key]
+
+    def match_prefix(self, searchString):
+
+        conn = sqlite3.connect(self.dbPath)
+        c = conn.cursor()
+
+        c.execute('select key from ' + self.tableName + ' where key like ?', (searchString + '%', ))
+
+        keys = []
+
+        for record in c.fetchall():
+            keys.append(record[0])
+
+        return keys
+
 
 class DB:
     def __init__(self, dir):
-        self.db = leveldb.LevelDB(appinfo.userdir + "/" + dir)
+        self.db = leveldb.LevelDB(appinfo.userdir + "/" + dir, max_open_files=200)
 
     def __getitem__(self, item):
         try:
@@ -95,14 +193,19 @@ class SongStore:
     def get(self, AcoustId):
         return self.songStore[AcoustId]
 
+    def update(self, AcoustId, data):
+        for d in data:
+            self.songStore.setAppend(AcoustId, d)
+
     def searchByArtist(self, searchString):
         keys = self.artistIndex.match_prefix(searchString)
         songs = []
 
         for key in keys:
             songSet = self.songStore[key]
-            for song in songSet:
-                songs.append(Song(song))
+            if songSet is not None:
+                for song in songSet:
+                    songs.append(Song(song))
 
         return songs
 
@@ -113,8 +216,9 @@ class SongStore:
 
         for key in keys:
             songSet = self.titleIndex[key]
-            for song in songSet:
-                songs.append(Song(song))
+            if songSet is not None:
+                for song in songSet:
+                    songs.append(Song(song))
 
         return songs
 
@@ -125,8 +229,9 @@ class SongStore:
 
         for key in keys:
             songSet = self.genreIndex[key]
-            for song in songSet:
-                songs.append(Song(song))
+            if songSet is not None:
+                for song in songSet:
+                    songs.append(Song(song))
 
         return songs
 
@@ -142,21 +247,37 @@ class SongStore:
 
 
     def getRandomSongs(self, oldSong=None, limit=1):
-        from random import sample
-
-        songs = []
-
         if oldSong is None:
-            keys = sample(self.songStore.match_prefix(''), limit)
-
-            for key in keys:
-                songSet = self.songStore[key]
-                for song in songSet:
-                    songs.append(Song(song))
-
-            return songs
+            return self._getRandomSongs(limit=limit)
         else:
-            songs = songs + self.searchByArtist(oldSong.artist)
-            songs = songs + self.searchByGenre(oldSong.genre)
+            songs = self._getRandomSongsBasedOnSong(oldSong=oldSong, limit=limit)
 
-            return sample(songs, limit)
+            if songs is []:
+                return self._getRandomSongs(limit=limit)
+
+    def _getRandomSongs(self, limit=1):
+        songs = []
+        keys = self._getRandomItemsFromList(self.songStore.match_prefix(''), limit)
+
+        for key in keys:
+            songSet = self.songStore[key]
+            for song in songSet:
+                songs.append(Song(song))
+
+        return songs
+
+    def _getRandomSongsBasedOnSong(self, oldSong=None, limit=1):
+        songs = []
+        songs = songs + self.searchByArtist(oldSong.artist)
+        songs = songs + self.searchByGenre(oldSong.genre)
+
+        return self._getRandomItemsFromList(songs, limit)
+
+    def _getRandomItemsFromList(self, list, numberToReturn=1):
+        if list is None or list is [] or numberToReturn < 1 or numberToReturn > len(list):
+            return []
+
+        from random import shuffle
+        shuffle(list)
+
+        return list[0:numberToReturn]
