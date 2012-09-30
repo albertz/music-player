24f98491e05f471d20a86bf9fa09b8ce61a3722d
Albert Zeyer <albzey@gmail.com>
2012-09-30 02:24:01 -0700

    debugging

--- test_acoustid.py
+++ test_acoustid.py
@@ -1,4 +1,5 @@
-
+import better_exchook
+better_exchook.install()
 
 class Song:
 	def __init__(self, fn):
@@ -17,14 +18,15 @@ if len(sys.argv) == 2:
 	filename = sys.argv[1]
 else:
 	files = [
-		"/Users/az/Music/Classic/Glenn Gould Plays Bach/Two- & Three-Part Inventions - Gould/19 Bach - Invention 13 in a (BWV 784).mp3",
-		"/Users/az/Music/Rock/Tool/Lateralus/09 Lateralus.flac",
-		"/Users/az/Music/Cults - Cults 7/Cults - Cults 7- - 03 The Curse.flac",
-		"/Users/az/Music/Special/zorba/(01) - Theme From Zorba The Greek.ogg",
-		"/Users/az/Music/Classic/Glenn Gould Plays Bach/French Suites, BWV812-7 - Gould/Bach, French Suite 5 in G, BWV816 - 5 Bourree.mp3",
-		"/Users/az/Music/Electronic/Von Paul Kalkbrenner - Aaron.mp3",
-		"/Users/az/Music/Electronic/One Day_Reckoning Song (Wankelmut Remix) - Asaf Avidan & the Mojos.mp3",
+		"~/Music/Classic/Glenn Gould Plays Bach/Two- & Three-Part Inventions - Gould/19 Bach - Invention 13 in a (BWV 784).mp3",
+		"~/Music/Rock/Tool/Lateralus/09 Lateralus.flac",
+		"~/Music/Cults - Cults 7/Cults - Cults 7- - 03 The Curse.flac",
+		"~/Music/Special/zorba/(01) - Theme From Zorba The Greek.ogg",
+		"~/Music/Classic/Glenn Gould Plays Bach/French Suites, BWV812-7 - Gould/Bach, French Suite 5 in G, BWV816 - 5 Bourree.mp3",
+		"~/Music/Electronic/Von Paul Kalkbrenner - Aaron.mp3",
+		"~/Music/Electronic/One Day_Reckoning Song (Wankelmut Remix) - Asaf Avidan & the Mojos.mp3",
 	]
+	files = map(os.path.expanduser, files)
 	filename = files[5]
 
 if len(sys.argv) >= 3:
