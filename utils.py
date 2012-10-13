331b1c19e622ed9ac70afc67915fae3679760fd2
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-10-13 04:10:03 +0200

    debugging

--- utils.py
+++ utils.py
@@ -557,3 +557,14 @@ def killMeHard():
 	import sys, os, signal
 	os.kill(0, signal.SIGKILL)
 	
+def dumpAllThreads():
+	import threading, sys, traceback
+	id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
+	code = []
+	for threadId, stack in sys._current_frames().items():
+		code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""), threadId))
+		for filename, lineno, name, line in traceback.extract_stack(stack):
+			code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
+			if line:
+				code.append("  %s" % (line.strip()))
+	print "\n".join(code)
