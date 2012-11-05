5aac9414daa96e033f94b8479d169e7da42142b7
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-11-05 06:59:02 +0100

    search

--- Search.py
+++ Search.py
@@ -1,5 +1,19 @@
 
+from utils import UserAttrib
+import Traits
+
 class Search:
-	pass
+	Keys = ("artist", "title", "duration", "rating")
+	_searchText = ""
+	
+	@UserAttrib(type=Traits.EditableText)
+	def searchText(self, updateText=None):
+		if updateText is not None:
+			self._searchText = updateText
+		return self._searchText
+	
+	@UserAttrib(type=Traits.Table(keys=Keys))
+	def searchResults(self):
+		pass
 
 search = Search()
--- Traits.py
+++ Traits.py
@@ -21,21 +21,9 @@ class List(TraitType):
 	def __getitem__(self, index): pass
 	def __len__(self): pass
 	
-class Table(TraitType):
-	def onInsert(self, index, value): pass
-	def onRemove(self, index): pass
-	def onClear(self): pass
-	
-	@property
-	@contextmanager
-	def lock(self): yield
-	
-	def insert(self, index, value): pass
-	def remove(self, index): pass
-	def clear(self): pass
-	def __getitem__(self, index): pass
-	def __len__(self): pass
-	
+class Table(List):
+	def __init__(self, keys):
+		self.keys = keys
 
 class EditableText(TraitType): pass
 class OneLineText(TraitType): pass
