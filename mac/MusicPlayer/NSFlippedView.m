0bb3bdbb933ddf3ba716dccf381041e9701bf24e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-01-04 02:10:59 +0100

    old code

--- mac/MusicPlayer/NSFlippedView.m
+++ mac/MusicPlayer/NSFlippedView.m
@@ -28,4 +28,69 @@
 	return YES;
 }
 
+//		def setDrawsBackground_(self, value):
+//			self._drawsBackground = value
+//			if value and not self._backgroundColor:
+//				self._backgroundColor = NSColor.whiteColor()
+//			self.setNeedsDisplay_(True)
+//		def setBackgroundColor_(self, value):
+//			self._backgroundColor = value
+//			if self._drawsBackground:
+//				self.setNeedsDisplay_(True)
+//		def backgroundColor(self): return self._backgroundColor
+//		def setDrawsFocusRing(self, value):
+//			self._drawsFocusRing = value
+//			self.setNeedsDisplay_(True)
+//		def isOpaque(self): return self._drawsBackground
+//		def drawRect_(self, dirtyRect):
+//			if self._drawsBackground:
+//				self._backgroundColor.setFill()
+//				NSRectFill(dirtyRect)
+//			if self._drawsFocusRing:
+//				NSSetFocusRingStyle(NSFocusRingOnly)
+//				NSRectFill(self.bounds())
+//		def acceptsFirstResponder(self):
+//			return utils.attrChain(self, "control", "attr", "canHaveFocus", default=False)
+//		def becomeFirstResponder(self):
+//			if NSView.becomeFirstResponder(self):
+//				if self.onBecomeFirstResponder: self.onBecomeFirstResponder()
+//				return True
+//			else:
+//				return False
+//		def resignFirstResponder(self):
+//			if NSView.resignFirstResponder(self):
+//				if self.onResignFirstResponder: self.onResignFirstResponder()				
+//				return True
+//			else:
+//				return False
+//		def keyDown_(self, ev):
+//			if not self.onKeyDown or not self.onKeyDown(ev):
+//				NSView.keyDown_(self, ev)
+//		def keyUp_(self, ev):
+//			if not self.onKeyUp or not self.onKeyUp(ev):
+//				NSView.keyUp_(self, ev)
+//		def mouseDown_(self, ev):
+//			if not self.onMouseDown or not self.onMouseDown(ev):
+//				NSView.mouseDown_(self, ev)
+//		def mouseDragged_(self, ev):
+//			if not self.onMouseDragged or not self.onMouseDragged(ev):
+//				NSView.mouseDragged_(self, ev)
+//		def mouseUp_(self, ev):
+//			if not self.onMouseUp or not self.onMouseUp(ev):
+//				NSView.mouseUp_(self, ev)
+//		def draggingEntered_(self, sender):
+//			if self.onDraggingEntered: self.onDraggingEntered(sender)
+//			return self.draggingUpdated_(sender)
+//		def draggingUpdated_(self, sender):
+//			if self.onDraggingUpdated: self.onDraggingUpdated(sender)
+//			return NSDragOperationGeneric
+//		def draggingExited_(self, sender):
+//			if self.onDraggingExited: self.onDraggingExited(sender)
+//		def prepareForDragOperation_(self, sender):
+//			return True
+//		def performDragOperation_(self, sender):
+//			if self.onPerformDragOperation and self.onPerformDragOperation(sender):
+//				return True
+//			return False
+
 @end
