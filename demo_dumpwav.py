65aa5a397b6080fcd4575d7d73135fb7b673e0bd
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-04-07 15:25:48 +0200

    demo_dumpwav: fixes

--- demo_dumpwav.py
+++ demo_dumpwav.py
@@ -10,12 +10,21 @@ import sys, os, time
 import ffmpeg
 player = ffmpeg.createPlayer()
 
+# Check if ffmpeg player is supported, i.e. version recent enough.
+assert hasattr(player, "soundcardOutputEnabled")
+assert hasattr(player, "nextSongOnEof")
+assert hasattr(player, "readOutStream")
+
+
+finished = False
+
 def onSongChange(*args, **kwargs):
-	print "song change", args, kwargs
+	# Don't load next song once the first one finished.
+	player.nextSongOnEof = False
 	
 def onSongFinished(*args, **kwargs):
-	print "song finished"
-	sys.exit()
+	global finished
+	finished = True
 	
 player.onSongChange = onSongChange
 player.onSongFinished = onSongFinished
@@ -24,10 +33,15 @@ class Song:
 	def __init__(self, fn):
 		self.url = fn
 		self.f = open(fn)
-	def __str__(self): return "Song(%s)" % os.path.basename(self.url)
-	def __eq__(self, other): return self.url == other.url
-	def readPacket(self, bufSize): return self.f.read(bufSize)
-	def seekRaw(self, offset, whence): return self.f.seek(offset, whence)
+	def __repr__(self):
+		return "<Song %s>" % os.path.basename(self.url)
+	def __eq__(self, other):
+		return self.url == other.url
+	def readPacket(self, bufSize):
+		return self.f.read(bufSize)
+	def seekRaw(self, offset, whence):
+		self.f.seek(offset, whence)
+		return self.f.tell()
 
 files = [
 	"~/Music/Electronic/28C3/[MA054] Dirk GlÅcksberg - Planet Red/03 Dirk GlÅcksberg - Found And Lost.mp3",
@@ -63,12 +77,43 @@ player.queue = songs()
 # This enables us to use use readOutStream.
 player.soundcardOutputEnabled = False
 
+
+
+def formatTime(t):
+	if t is None: return "?"
+	mins = long(t // 60)
+	t -= mins * 60
+	hours = mins // 60
+	mins -= hours * 60
+	if hours: return "%02i:%02i:%02.0f" % (hours,mins,t)
+	return "%02i:%02.0f" % (mins,t)
+
+def printPos():
+	try:
+		proc = 100.0 * player.curSongPos / player.curSongLen
+	except TypeError:
+		proc = 0
+	sys.stdout.write("%r: %i%%, %s / %s" % ( 
+		player.curSong,
+		proc,
+		formatTime(player.curSongPos),
+		formatTime(player.curSongLen)
+	))
+	sys.stdout.flush()
+
 # Start the encoder thread.
 player.playing = True
 
-
-while True:
-	time.sleep(1)
-	#buf = player.readOutStream()
+printPos()
+while not finished:
+	buf = player.readOutStream()
+	if len(buf) == 0:
+		# Let's wait a bit to fill the stream.
+		time.sleep(0.1)
+		continue
 	#print len(buf), repr(buf[0:20])
 
+	sys.stdout.write("\r\033[K") # clear line
+	printPos()
+print
+print "song finished"
