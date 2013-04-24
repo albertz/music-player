ff3ff57367efe505ad5c65f0696a26984cce6163
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-04-24 13:09:59 +0200

    addsong-bottom.py

---
+++ tools/addsong-bottom.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# MusicPlayer, https://github.com/albertz/music-player
+# Copyright (c) 2013, Albert Zeyer, www.az2000.de
+# All rights reserved.
+# This code is under the 2-clause BSD license, see License.txt in the root directory of this project.
+
+import sys, os
+
+importPath = os.path.join(os.path.dirname(__file__), "..")
+if importPath not in sys.path:
+	sys.path += [importPath]
+
+def addSongsBottom(files):
+	import shell
+	shell.connect()
+	shell.remoteExec("from Song import Song")
+	for fn in files:
+		fn = os.path.abspath(fn)
+		assert os.path.exists(fn), "file %r does not exist" % fn
+		shell.remoteExec("state.queue.queue.append(Song(%r))" % fn)
+
+if __name__ == "__main__":
+	import better_exchook
+	better_exchook.install()
+
+	from appinfo_args import argParser
+	argParser.parse_args = lambda: None
+
+	files = sys.argv[1:]
+	assert files, "usage: %s <files>" % sys.argv[0]
+	assert all([os.path.exists(fn) for fn in files]), "some files do not exist"
+
+	addSongsBottom(files)
+
