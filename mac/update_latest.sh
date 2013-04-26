8f2215e9538544874cf2568eb719e704c005ec6b
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-04-26 21:58:26 +0200

    update_latest.sh

--- mac/update_latest.sh
+++ mac/update_latest.sh
@@ -1,12 +1,23 @@
-#!/bin/bash
+#!/bin/zsh
 
 cd "$(dirname "$0")"
 
-xcodebuild || exit 1
+#xcodebuild || exit 1
+
 
 pushd build/Release || exit 1
-rm MusicPlayer-latest.zip
-zip -9 -r MusicPlayer-latest.zip MusicPlayer.app || exit 1
+
+zipfilename="MusicPlayer-MacApp-$(date "+%Y%m%d")"
+i=1
+for f in $zipfilename-*.zip; do
+	j=$(echo $f | sed -e "s/${zipfilename}-\([0-9]*\)-.*\\.zip/\1/")
+	[[ $j -ge $i ]] && i=$(expr $j + 1)
+done
+zipfilename="$zipfilename-$i-$(git describe --always).zip"
+
+rm MusicPlayer-latest*.zip
+zip -9 -r $zipfilename MusicPlayer.app || exit 1
+ln -s $zipfilename MusicPlayer-MacApp-latest.zip
 popd
 
-ruby github-upload.rb build/Release/MusicPlayer-latest.zip
+rsync -avP build/Release/*.zip albertzeyer@frs.sourceforge.net:/home/frs/project/az-music-player/
