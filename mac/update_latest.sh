b1588a986f91fe837d599b936892d0be206cbfb4
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-04-27 14:38:35 +0200

    more

--- mac/update_latest.sh
+++ mac/update_latest.sh
@@ -6,9 +6,10 @@ gitrev="$(git show HEAD  --format=format:%h | head -n1)" || exit 1
 
 #xcodebuild || exit 1
 
-pushd build/Release || exit 1
+pushd build/Release || { echo "build/Release does not exist"; exit 1; }
 
-[ *"-$gitrev.zip" != "" ] && { echo "package exists:" *"-$gitrev.zip"; exit 1; }
+[ *"-$gitrev.zip" != "" ] 2>/dev/null && \
+	{ echo "package exists:" *"-$gitrev.zip"; exit 1; }
 
 zipfilename="MusicPlayer-MacApp-$(date "+%Y%m%d")"
 i=1
@@ -21,8 +22,12 @@ zipfilename="$zipfilename-$i-$gitrev.zip"
 #rm MusicPlayer-MacApp-20*.zip # toodo...
 zip -9 -r $zipfilename MusicPlayer.app || exit 1
 
-rm MusicPlayer-MacApp-latest.zip
-ln -s $zipfilename MusicPlayer-MacApp-latest.zip
+dfile="download_macapp_latest.php"
+echo "<html><head><title>MusicPlayer</title></head><body>" >$dfile
+echo "<?php echo 'foo'; ?> bar</body></html>" >>$dfile
+
+#ln -s $zipfilename MusicPlayer-MacApp-latest.zip
 popd
 
 rsync -avP build/Release/*.zip albertzeyer@frs.sourceforge.net:/home/frs/project/az-music-player/
+rsync -avP build/Release/$dfile albertzeyer,az-music-player@web.sourceforge.net:htdocs/
