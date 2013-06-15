56714698386c2a88cec29377d5392c670048eadd
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-06-15 17:24:33 +0200

    rpython ref

--- test_rpython.sh
+++ test_rpython.sh
@@ -5,6 +5,7 @@ RPYTHONDIR=~/Downloads/pypy-2.0.2-src
 
 # compile <file.py> <file.exe>
 function compile() {
+	# see: http://www.rfk.id.au/blog/entry/compiling-rpython-programs/
 	export PYTHONPATH=$RPYTHONDIR
 	$RPYTHONDIR/rpython/translator/goal/translate.py \
 	--batch --output $2 $1
