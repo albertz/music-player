83a8779320cf0438537b09f9a98a1a1bb5baa648
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-08-03 19:45:45 +0200

    debugger initial

---
+++ debugger/debugger.cpp
@@ -0,0 +1,9 @@
+//
+//  debugger.cpp
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 03.08.13.
+//  Copyright (c) 2013 Albert Zeyer. All rights reserved.
+//
+
+#include "debugger.h"
---
+++ debugger/debugger.h
@@ -0,0 +1,14 @@
+//
+//  debugger.h
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 03.08.13.
+//  Copyright (c) 2013 Albert Zeyer. All rights reserved.
+//
+
+#ifndef __MusicPlayer__debugger__
+#define __MusicPlayer__debugger__
+
+#include <iostream>
+
+#endif /* defined(__MusicPlayer__debugger__) */
