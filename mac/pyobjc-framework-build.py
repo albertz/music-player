a1dbc3b0c7df2446e51563e8becc3b77ea4670eb
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-11-07 16:30:35 +0100

    finish building

--- mac/pyobjc-framework-build.py
+++ mac/pyobjc-framework-build.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import sys
+import shutil
+import subprocess
 sys.path = ["."] + sys.path
 os.chdir(os.path.dirname(__file__))
 
-try: os.mkdir("build/Release/pyobjc")
-except OSError: pass # probably already exists
-finally: assert os.path.exists("build/Release/pyobjc")
+TargetDir = "build/Release/pyobjc"
+try: shutil.rmtree(TargetDir)
+except OSError: pass
+os.mkdir(TargetDir)
+assert os.path.exists(TargetDir)
+
+
+def copytree(src, dst, symlinks=False):
+	try: os.makedirs(dst)
+	except OSError: pass # probably exists or so
+	names = os.listdir(src)
+	for name in names:
+		srcname = os.path.join(src, name)
+		dstname = os.path.join(dst, name)
+		if symlinks and os.path.islink(srcname):
+			linkto = os.readlink(srcname)
+			os.symlink(linkto, dstname)
+		elif os.path.isdir(srcname):
+			copytree(srcname, dstname, symlinks)
+		else:
+			# Will raise a SpecialFileError for unsupported file types
+			shutil.copy2(srcname, dstname)
+
 
 setup_kwargs = None
 def _catch_setup(**kwargs):
@@ -22,12 +45,46 @@ for framework in ["Cocoa", "Quartz"]:
 		os.chdir(fDir)
 		import pyobjc_setup
 		pyobjc_setup.setup = _catch_setup
+
+		origExtension = pyobjc_setup._Extension
+		def MyExtension(*args, **kwds):
+			for attr in ["extra_compile_args", "extra_link_args"]:
+				flags = kwds[attr]
+				idx = flags.index("-isysroot")
+				flags[idx:idx+2] = []
+			return origExtension(*args, **kwds)
+		pyobjc_setup._Extension = MyExtension
+
 		import setup
 		assert setup_kwargs is not None
 
-		print setup_kwargs
+		for package in setup_kwargs["packages"]:
+			print "*** package:", package
+			package = package.replace(".", "/")
+			copytree(
+				"Lib/" + package,
+				"../" + TargetDir + "/" + package)
+
+		for extmod in setup_kwargs["ext_modules"]:
+			if extmod.name.split(".")[0] == "PyObjCTest": continue # ignore
+
+			print "*** ext module:", extmod.name
+			filename = extmod.name.replace(".", "/") + ".so"
+			args = \
+				["cc"] + \
+				extmod.sources + \
+				["-I../../python-embedded/CPython/Include",
+				 "-I../../python-embedded/pylib",
+				 "-I../pyobjc-core/Modules/objc"] + \
+				extmod.extra_compile_args + \
+				extmod.extra_link_args + ["-undefined", "dynamic_lookup"] + \
+				["-o", "../" + TargetDir + "/" + filename]
+			print " ".join(args)
+			res = subprocess.call(args)
+			if res != 0: sys.exit(res)
+
 		sys.exit(0)
-		
+
 	else:
 		_, res = os.waitpid(pid, 0)
 		if res != 0: sys.exit(res)
