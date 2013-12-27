605b6a9e78dfdec9bab329f176a43185a8698062
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-12-27 21:55:25 +0100

    first test case

--- tests/test_LinkedList.cpp
+++ tests/test_LinkedList.cpp
@@ -1,7 +1,27 @@
 
-
+#include <assert.h>
 #include "LinkedList.hpp"
 
-int main() {
+void test1() {
+	LinkedList<int> l;
+	l._check_sanity();
 
+	for(int i = 0; i < 100; ++i) {
+		auto item = l.push_back();
+		l._check_sanity();
+		item->value = i;
+	}
+
+	for(int i = 0; i < 100; ++i) {
+		auto ret = l.pop_front();
+		l._check_sanity();
+		assert(ret);
+		assert(ret->value == i);
+	}
+
+	assert(l.empty());
+}
+
+int main() {
+	test1();
 }
