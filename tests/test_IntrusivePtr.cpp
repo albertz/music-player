f0b8f65d843e9aec36f33c15dc6577a63f8e974e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-12-28 01:11:22 +0100

    reproduce error

---
+++ tests/test_IntrusivePtr.cpp
@@ -0,0 +1,36 @@
+
+#include <boost/smart_ptr/intrusive_ref_counter.hpp>
+#include "IntrusivePtr.hpp"
+
+struct Item;
+typedef IntrusivePtr<Item> ItemPtr;
+
+struct Item : public boost::intrusive_ref_counter< Item, boost::thread_safe_counter > {
+	ItemPtr next;
+	int value;
+
+	Item(int v = 0) : value(v) {}
+};
+
+
+int main() {
+	IntrusivePtr<Item> p1(new Item(12));
+	IntrusivePtr<Item> p2(new Item(23));
+	IntrusivePtr<Item> p3(new Item(34));
+
+	p2 = p1;
+	p2 = NULL;
+	p2 = p1;
+
+	p2->next = p3;
+
+	for(int i = 0; i < 100; ++i) {
+		ItemPtr oldFirst = p2;
+		if(p2)
+			p2 = p2->next;
+
+		p2 = NULL;
+		p1 = p2;
+	}
+
+}
