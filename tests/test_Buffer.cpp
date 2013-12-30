f8d288a9f6c7d0571ac22414cd04c102a353419d
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-12-30 04:03:02 +0100

    initial test_Buffer

---
+++ tests/test_Buffer.cpp
@@ -0,0 +1,42 @@
+
+#include "Buffer.cpp"
+
+#include <thread>
+
+void test1() {
+	Buffer buf;
+
+	auto producer = [&buf](){
+		for(int i = 0; i < 100; ++i) {
+			buf.push((uint8_t*)&i, sizeof(i));
+		}
+	};
+
+	auto consumer = [&buf](){
+		for(int i = 0; i < 100; ++i) {
+			int ret;
+			while(buf.size() > sizeof(ret)); // wait for entry
+			size_t c = buf.pop((uint8_t*)&ret, sizeof(ret));
+			assert(c == sizeof(ret));
+			assert(ret == i);
+		}
+	};
+
+	for(int i = 0; i < 1000; ++i) {
+		producer();
+		assert(buf.size() == 100);
+		consumer();
+		assert(buf.empty());
+	}
+
+	for(int i = 0; i < 1000; ++i) {
+		std::thread t1(producer), t2(consumer);
+		t1.join();
+		t2.join();
+		assert(buf.empty());
+	}
+}
+
+int main() {
+	test1();
+}
