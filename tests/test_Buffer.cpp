3b277f2d85e39fc3452f52a130649ef9e08da231
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-12-30 04:36:22 +0100

    more on buffer test

--- tests/test_Buffer.cpp
+++ tests/test_Buffer.cpp
@@ -7,11 +7,15 @@ void test1() {
 	Buffer buf;
 
 #define N 10000
-	auto producer = [&buf](){
-		for(uint32_t i = 0; i < N; ++i) {
+	// We want to force multiple buffer chunks.
+	assert(N * sizeof(uint32_t) > BUFFER_CHUNK_SIZE);
+
+	auto producer_ = [&buf](int start, int end){
+		for(uint32_t i = start; i < end; ++i) {
 			buf.push((uint8_t*)&i, sizeof(uint32_t));
 		}
 	};
+	auto producer = [&]() { producer_(0, N); };
 
 	auto consumer = [&buf](){
 		for(uint32_t i = 0; i < N; ++i) {
@@ -32,6 +36,14 @@ void test1() {
 
 	for(int i = 0; i < 1000; ++i) {
 		std::thread t1(producer), t2(consumer);
+		t1.join();
+		t2.join();
+		assert(buf.empty());
+	}
+
+	for(int i = 0; i < 1000; ++i) {
+		producer_(0, N/2);
+		std::thread t1([&](){ producer_(N/2, N); }), t2(consumer);
 		t1.join();
 		t2.join();
 		assert(buf.empty());
