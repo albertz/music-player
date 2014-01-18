8011fbb9b5e0d52a79d043b0f2090b31e1d6c585
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-18 14:30:05 +0100

    some dummy implementations

--- _gui/GuiObject.cpp
+++ _gui/GuiObject.cpp
@@ -1,13 +1,23 @@
 #include "GuiObject.hpp"
 
 int GuiObject::init(PyObject* args, PyObject* kwds) {
-	
+	return 0;
 }
 
 PyObject* GuiObject::getattr(const char* key) {
+	PyErr_Format(PyExc_AttributeError, "GuiObject has no attribute '%.400s'", key);
+	return NULL;
 	
+returnNone:
+	Py_INCREF(Py_None);
+	return Py_None;
 }
 
 int GuiObject::setattr(const char* key, PyObject* value) {
-	
+	PyObject* s = PyString_FromString(key);
+	if(!s) return -1;
+	// While we have no own __dict__, this will fail. But that is what we want.
+	int ret = PyObject_GenericSetAttr((PyObject*) this, s, value);
+	Py_XDECREF(s);
+	return ret;
 }
