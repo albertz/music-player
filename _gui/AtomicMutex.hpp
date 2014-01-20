6aa8d7f2b0673defd1a1aca1bd2753537c5cfc73
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-20 11:41:36 +0100

    AtomicMutex and SafeValue

--- _gui/AtomicMutex.hpp
+++ _gui/AtomicMutex.hpp
@@ -1,14 +1,35 @@
 //
-//  AtomicLock.hpp
+//  AtomicMutex.hpp
 //  MusicPlayer
 //
 //  Created by Albert Zeyer on 20.01.14.
 //  Copyright (c) 2014 Albert Zeyer. All rights reserved.
 //
 
-#ifndef MusicPlayer_AtomicLock_hpp
-#define MusicPlayer_AtomicLock_hpp
+#ifndef MusicPlayer_AtomicMutex_hpp
+#define MusicPlayer_AtomicMutex_hpp
 
+#include <boost/atomic.hpp>
+#include <boost/noncopyable.hpp>
 
+class AtomicMutex {
+	boost::atomic<bool> lock;
+
+public:
+	struct Scope : boost::noncopyable {
+		AtomicMutex& m;
+		Scope(AtomicMutex& _m) : m(_m) {
+			while(true) {
+				bool expected = false;
+				if(m.lock.compare_exchange_weak(expected, true))
+					break;
+			}
+		}
+		~Scope() {
+			bool oldLock = m.lock.exchange(false);
+			assert(oldLock);
+		}
+	};
+};
 
 #endif
--- _gui/SafeValue.hpp
+++ _gui/SafeValue.hpp
@@ -1,14 +1,49 @@
 //
-//  SafePtr.hpp
+//  SafeValue.hpp
 //  MusicPlayer
 //
 //  Created by Albert Zeyer on 20.01.14.
 //  Copyright (c) 2014 Albert Zeyer. All rights reserved.
 //
 
-#ifndef MusicPlayer_SafePtr_hpp
-#define MusicPlayer_SafePtr_hpp
+#ifndef MusicPlayer_SafeValue_hpp
+#define MusicPlayer_SafeValue_hpp
 
+#include "AtomicMutex.hpp"
+#include <boost/atomic.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/function.hpp>
+#include <assert.h>
 
+template<typename T>
+class SafeValue {
+	AtomicMutex mutex;
+	T value;
+
+public:
+	void set(const T& _v) {
+		AtomicMutex::Scope lock(mutex);
+		value = _v;
+	}
+
+	T get() {
+		AtomicMutex::Scope lock(mutex);
+		return value;
+	}
+
+	struct ScopedLock {
+		AtomicMutex::Scope lock;
+		T& value;
+		ScopedLock(SafeValue& _v) : lock(_v.mutex), value(_v.value) {}
+	};
+	
+	void operate(boost::function<void(T&)> op) {
+		AtomicMutex::Scope lock(mutex);
+		op(value);
+	}
+	
+	SafeValue& operator=(const T& _v) { set(_v); return *this; }
+	operator T() { return get(); }
+};
 
 #endif
