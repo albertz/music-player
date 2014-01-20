26913935e5391f448695c16ab8fd441e8b8e2c47
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-20 11:40:47 +0100

    initial SafeValue and AtomicMutex

---
+++ _gui/AtomicMutex.hpp
@@ -0,0 +1,14 @@
+//
+//  AtomicLock.hpp
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 20.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#ifndef MusicPlayer_AtomicLock_hpp
+#define MusicPlayer_AtomicLock_hpp
+
+
+
+#endif
---
+++ _gui/SafeValue.hpp
@@ -0,0 +1,14 @@
+//
+//  SafePtr.hpp
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 20.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#ifndef MusicPlayer_SafePtr_hpp
+#define MusicPlayer_SafePtr_hpp
+
+
+
+#endif
