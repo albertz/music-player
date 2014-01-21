9483d085d7bcf47b64d291f02193fb630ebeb141
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-21 13:59:09 +0100

    more

---
+++ mac/gui/ControlWithChilds.hpp
@@ -0,0 +1,13 @@
+//
+//  ControlWithChilds.h
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 21.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ControlWithChilds <NSObject>
+
+@end
