0966d90330b2e4798616b676560cb3eb11418714
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-21 10:17:47 +0100

    prepare to implement list control

---
+++ mac/gui/ListControl.h
@@ -0,0 +1,13 @@
+//
+//  ListControl.h
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 21.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ListControl : NSView
+
+@end
---
+++ mac/gui/ListControl.m
@@ -0,0 +1,29 @@
+//
+//  ListControl.m
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 21.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#import "ListControl.h"
+
+@implementation ListControl
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
