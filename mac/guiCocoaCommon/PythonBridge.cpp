df48c629ac052f789eaf9ff5c0c50fca3aabaa69
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-21 10:16:55 +0100

    cleanup

--- mac/guiCocoaCommon/PythonBridge.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  PythonBridge.cpp
-//  MusicPlayer
-//
-//  Created by Albert Zeyer on 07.11.13.
-//  This code is under the 2-clause BSD license, see License.txt in the root directory of this project.
-//
-
-#include "PythonBridge.h"
