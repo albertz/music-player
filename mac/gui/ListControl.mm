f7db6bc1a93dd5f4089dc586e4fb5b682bc7b90b
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-22 15:45:22 +0100

    fix: if there is nothing to traverse (via tp_traverse), dont set Py_TPFLAGS_HAVE_GC at all

--- mac/gui/ListControl.mm
+++ mac/gui/ListControl.mm
@@ -88,7 +88,7 @@ PyTypeObject FunctionWrapper_Type = {
     0,											/* tp_getattro */
     0,                                          /* tp_setattro */
     0,                                          /* tp_as_buffer */
-    Py_TPFLAGS_HAVE_CLASS | Py_TPFLAGS_HAVE_WEAKREFS | Py_TPFLAGS_HAVE_GC,/* tp_flags */
+    Py_TPFLAGS_HAVE_CLASS | Py_TPFLAGS_HAVE_WEAKREFS,/* tp_flags */
     0,                                          /* tp_doc */
     0,											/* tp_traverse */
     0,											/* tp_clear */
