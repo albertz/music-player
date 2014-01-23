960712bb2cf5abec3e12cb4d6d8a7f8a303f3ad9
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-23 14:26:36 +0100

    move FunctionWrapper code

---
+++ mac/gui/FunctionWrapper.cpp
@@ -0,0 +1,9 @@
+//
+//  FunctionWrapper.cpp
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 23.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#include "FunctionWrapper.h"
---
+++ mac/gui/FunctionWrapper.hpp
@@ -0,0 +1,14 @@
+//
+//  FunctionWrapper.h
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 23.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#ifndef __MusicPlayer__FunctionWrapper__
+#define __MusicPlayer__FunctionWrapper__
+
+#include <iostream>
+
+#endif /* defined(__MusicPlayer__FunctionWrapper__) */
