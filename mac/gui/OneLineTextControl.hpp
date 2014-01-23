cc45f91ecb10365bacf22f0f28187076463d8d7e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-23 22:09:48 +0100

    OneLineTextControl

---
+++ mac/gui/OneLineTextControl.hpp
@@ -0,0 +1,13 @@
+//
+//  OneLineTextControl.h
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 23.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#import "GuiObjectView.hpp"
+
+@interface OneLineTextControl : GuiObjectView
+
+@end
---
+++ mac/gui/OneLineTextControl.mm
@@ -0,0 +1,29 @@
+//
+//  OneLineTextControl.m
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 23.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#import "OneLineTextControl.h"
+
+@implementation OneLineTextControl
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
