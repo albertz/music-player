7bb1f7984289c4c881d0e336b867b7ee40b15d27
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-23 12:57:28 +0100

    restructure some code. builder functions

---
+++ mac/gui/Builders.hpp
@@ -0,0 +1,14 @@
+//
+//  Builders.hpp
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 23.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#ifndef MusicPlayer_Builders_hpp
+#define MusicPlayer_Builders_hpp
+
+
+
+#endif
