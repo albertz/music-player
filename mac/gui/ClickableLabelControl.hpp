b7b8736c9f2f10e6d2f220387af7d9198d74db31
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-24 09:32:13 +0100

    ClickableLabelControl

---
+++ mac/gui/ClickableLabelControl.hpp
@@ -0,0 +1,13 @@
+//
+//  ClickableLabelControl.h
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 24.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#import "OneLineTextControl.hpp"
+
+@interface ClickableLabelControl : OneLineTextControlView
+
+@end
---
+++ mac/gui/ClickableLabelControl.mm
@@ -0,0 +1,29 @@
+//
+//  ClickableLabelControl.m
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 24.01.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#import "ClickableLabelControl.h"
+
+@implementation ClickableLabelControl
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
