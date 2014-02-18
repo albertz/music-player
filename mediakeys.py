28867ad73aa867cfde92815c18b04006848c4110
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-02-18 09:22:22 +0100

    small fix

--- mediakeys.py
+++ mediakeys.py
@@ -129,7 +129,7 @@ def onMediaKeyUp(control):
 			state.playPause()
 		elif control == "next":
 			state.nextSong()
-	except:
+	except Exception:
 		sys.excepthook(*sys.exc_info())
 
 def mediakeysMain():
