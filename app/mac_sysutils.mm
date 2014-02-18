0db5c1759d581818bf0b96f9addba0fd44c9589d
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-02-18 16:05:44 +0100

    fix __NSAutoreleaseNoPool overwriting

--- app/mac_sysutils.mm
+++ app/mac_sysutils.mm
@@ -15,6 +15,8 @@
 #include <Foundation/NSDebug.h>
 #include <execinfo.h>
 #include <dlfcn.h>
+#include <mach-o/dyld.h>
+#include <mach-o/nlist.h>
 
 
 // for AmIBeingDebugged
@@ -124,6 +126,81 @@ void handleFatalError(const char* msg) {
 	_exit(1);
 }
 
+
+// Adapted from:
+// https://github.com/0xced/iOS-Artwork-Extractor/blob/master/Classes/FindSymbol.c
+// Adapted from MoreAddrToSym / GetFunctionName()
+// http://www.opensource.apple.com/source/openmpi/openmpi-8/openmpi/opal/mca/backtrace/darwin/MoreBacktrace/MoreDebugging/MoreAddrToSym.c
+void *FindSymbol(const struct mach_header *img, const char *symbol)
+{
+    if ((img == NULL) || (symbol == NULL))
+        return NULL;
+
+    // only 64bit supported
+#if defined (__LP64__)
+
+    if(img->magic != MH_MAGIC_64)
+        // we currently only support Intel 64bit
+        return NULL;
+
+    struct mach_header_64 *image = (struct mach_header_64*) img;
+
+    struct segment_command_64 *seg_linkedit = NULL;
+    struct segment_command_64 *seg_text = NULL;
+    struct symtab_command *symtab = NULL;
+    unsigned int index;
+
+    struct load_command *cmd = (struct load_command*)(image + 1);
+
+    for (index = 0; index < image->ncmds; index += 1, cmd = (struct load_command*)((char*)cmd + cmd->cmdsize))
+    {
+        switch(cmd->cmd)
+        {
+            case LC_SEGMENT_64: {
+                struct segment_command_64* segcmd = (struct segment_command_64*)cmd;
+                if (!strcmp(segcmd->segname, SEG_TEXT))
+                    seg_text = segcmd;
+                else if (!strcmp(segcmd->segname, SEG_LINKEDIT))
+                    seg_linkedit = segcmd;
+                break;
+            }
+
+            case LC_SYMTAB:
+                symtab = (struct symtab_command*)cmd;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    if ((seg_text == NULL) || (seg_linkedit == NULL) || (symtab == NULL))
+        return NULL;
+
+    unsigned long vm_slide = (unsigned long)image - (unsigned long)seg_text->vmaddr;
+    unsigned long file_slide = ((unsigned long)seg_linkedit->vmaddr - (unsigned long)seg_text->vmaddr) - seg_linkedit->fileoff;
+    struct nlist_64 *symbase = (struct nlist_64*)((unsigned long)image + (symtab->symoff + file_slide));
+    char *strings = (char*)((unsigned long)image + (symtab->stroff + file_slide));
+    struct nlist_64 *sym;
+
+    for (index = 0, sym = symbase; index < symtab->nsyms; index += 1, sym += 1)
+    {
+        if (sym->n_un.n_strx != 0 && !strcmp(symbol, strings + sym->n_un.n_strx))
+        {
+            unsigned long address = vm_slide + sym->n_value;
+            if (sym->n_desc & N_ARM_THUMB_DEF)
+                return (void*)(address | 1);
+            else
+                return (void*)(address);
+        }
+    }   
+#endif
+
+    return NULL;
+}
+
+
+
 void print_backtrace() {
     void *callstack[128];
     int framesC = backtrace(callstack, sizeof(callstack));
@@ -167,16 +244,14 @@ void __NSAutoreleaseNoPool_replacement(void* obj) {
 void install_breakpoint_handlers() {
 	kern_return_t err = 0;
 	
-	// TODO: This does not work. It doesn't find the symbol.
-	// http://stackoverflow.com/questions/21849208/how-can-i-get-the-nsautoreleasenopool-address
-	NSAutoreleaseNoPoolFunc __NSAutoreleaseNoPool = (NSAutoreleaseNoPoolFunc) dlsym(RTLD_DEFAULT, "__NSAutoreleaseNoPool");
+	const struct mach_header* img = NSAddImage("/System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation", NSADDIMAGE_OPTION_NONE);
+	NSAutoreleaseNoPoolFunc __NSAutoreleaseNoPool = (NSAutoreleaseNoPoolFunc) FindSymbol((struct mach_header*)img, "___NSAutoreleaseNoPool");
+	// Note that __NSAutoreleaseNoPool does not exist on newer MacOSX versions. I guess it got removed with ARC.
 	if(__NSAutoreleaseNoPool)
 		err = mach_override_ptr(
 			(void*)__NSAutoreleaseNoPool,
 			(void*)__NSAutoreleaseNoPool_replacement,
 			(void**)&__NSAutoreleaseNoPool_reenter);
-	//if(!__NSAutoreleaseNoPool)
-	//	printf("__NSAutoreleaseNoPool not found\n");
 	
 	// Enable some further Cocoa debugging.
 	NSDebugEnabled = true;
