eb5102893494c359e1e9cf546547ad037f295f15
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-02-19 15:09:59 +0100

    header

--- app/sysutils.hpp
+++ app/sysutils.hpp
@@ -1,8 +1,12 @@
-#ifndef __MusicPlayer_app_sysutils_h__
-#define __MusicPlayer_app_sysutils_h__
+#ifndef __MusicPlayer_app_sysutils_hpp__
+#define __MusicPlayer_app_sysutils_hpp__
 
-#ifdef __cplusplus
+#ifndef __cplusplus
+#error C++ only
+#endif
+
 #include <string>
+#include <boost/function.hpp>
 
 extern bool logEnabled;
 extern std::string logFilename;
@@ -12,21 +16,27 @@ std::string getTildeExpandedPath(const std::string& path);
 bool fileExists(const std::string& path);
 
 extern "C" {
-#endif
 
-extern const char* StartupStr;
-
-__attribute__((visibility("default")))
-bool AmIBeingDebugged();
-
-__attribute__((visibility("default")))
-void print_backtrace(int bInSignalHandler);
-
-__attribute__((visibility("default")))
-void handleFatalError(const char* msg);
-
-#ifdef __cplusplus
+	extern const char* StartupStr;
+	
+	__attribute__((visibility("default")))
+	bool AmIBeingDebugged();
+	
+	__attribute__((visibility("default")))
+	void print_backtrace(int bInSignalHandler);
+	
+	__attribute__((visibility("default")))
+	void handleFatalError(const char* msg);
+	
 }
-#endif
+
+// debugging stuff
+
+void* GetPCFromUContext(void* ucontext);
+
+typedef long ThreadId;
+int GetCallstack(ThreadId threadId, void **buffer, int size);
+void ExecInThread(ThreadId threadId, boost::function<void(int signum, void* siginfo, void* sigsecret)> func);
+
 
 #endif
