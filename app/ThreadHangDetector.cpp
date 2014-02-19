1b089ca4d5084fdabad47fe4b3d4d508d372c33e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-02-19 15:09:20 +0100

    add ThreadHangDetector

---
+++ app/ThreadHangDetector.cpp
@@ -0,0 +1,9 @@
+//
+//  ThreadHangDetector.cpp
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 19.02.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#include "ThreadHangDetector.h"
---
+++ app/ThreadHangDetector.hpp
@@ -0,0 +1,14 @@
+//
+//  ThreadHangDetector.h
+//  MusicPlayer
+//
+//  Created by Albert Zeyer on 19.02.14.
+//  Copyright (c) 2014 Albert Zeyer. All rights reserved.
+//
+
+#ifndef __MusicPlayer__ThreadHangDetector__
+#define __MusicPlayer__ThreadHangDetector__
+
+#include <iostream>
+
+#endif /* defined(__MusicPlayer__ThreadHangDetector__) */
