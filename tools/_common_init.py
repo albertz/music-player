ac482510113902cb5e5d50beba8cf58491d3341d
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-05-22 17:43:25 +0200

    common init, small fix

--- tools/_common_init.py
+++ tools/_common_init.py
@@ -2,7 +2,7 @@
 import sys, os
 
 importPath = os.path.join(os.path.dirname(__file__), "../src")
-assert os.path.exists("better_exchook.py"), "MusicPlayer source path seems invalid: %r" % importPath
+assert os.path.exists("%s/better_exchook.py" % importPath), "MusicPlayer source path seems invalid: %r" % importPath
 
 if importPath not in sys.path:
 	sys.path += [importPath]
